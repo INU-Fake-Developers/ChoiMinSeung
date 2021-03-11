c79a0ae45da12783a1a56bd73d85805aff256fcd
ChoiMinSeung <79641973+ChoiMinSeung@users.noreply.github.com>
2021-03-11 22:02:39 +0900

    Update main.py

--- pyCharmXgithub/main.py
+++ pyCharmXgithub/main.py
@@ -1,9 +1,4 @@
-def temperature():
-    temp = int(input("오늘의 온도 : "))
-    if temp >= 35:
-        print("더워죽겠다")
-    elif temp > 10:
-        print("살만한 날씨구나")
-    else:
-        print("추운 날씨다")
-temperature()
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
